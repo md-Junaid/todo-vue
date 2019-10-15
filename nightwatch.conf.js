b6cc95e98267dff1839373f2857082f12ef6bdc8
Junaid <mohammed@wobe-team.com>
2019-10-15 16:56:29 +0200

    tests done

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,6 +1,8 @@
 const chromedriver = require("chromedriver");
+const geckodriver = require("geckodriver");
 module.exports = (function (settings) {
   settings.test_workers = false;
-  settings.webdriver.server_path = chromedriver.path;
+  settings.test_settings.default.webdriver.server_path = chromedriver.path;
+  settings.test_settings.firefox.webdriver.server_path = geckodriver.path;
   return settings;
 })(require("./nightwatch.json"));
--- tests/firstTest.js
+++ tests/firstTest.js
@@ -8,28 +8,33 @@
 // }
 
 module.exports = {
-    'step one: navigate to app' : function (browser) {
+    'step one: write tasks' : function (browser) {
       browser
         .url('localhost:8080')
-        .waitForElementVisible('#app', 1000)
+        .waitForElementVisible('div#nav', 200)
         .setValue('input[type=text]', 'Testing Nightwatch here')
-        .waitForElementVisible('input[type=submit]', 1000)
+        .click('input[type=submit]')
+        .setValue('input[type=text]', 'This task is done')
+        .click('input[type=submit]')
+        .setValue('input[type=text]', 'This task is done too')
+        .click('input[type=submit]')
+        .setValue('input[type=text]', 'This task is not done')
+        .click('input[type=submit]')
     },
   
-    'step two: click submit' : function (browser) {
+    'step two: checklist done' : function (browser) {
       browser
-        .click('input[type=submit]')
-        .pause(1000)
-        .elements('css selector', 'div.todo-item', function(result) {
-            console.log('result value is:', result);
-            // this.assert.containsText('p', 'Testing Nightwatch here') 
-            if(result.value === 'Testing Nightwatch here') {
-                console.log('My test code is working');
-            }           
+        .click('div > div:nth-of-type(7) > div > p > input[type=checkbox]')
+        .click('div > div:nth-of-type(8) > div > p > input[type=checkbox]')
+        .click('div > div:nth-of-type(4) > div > p > button')
+        .assert.containsText('div > div:nth-of-type(6) > div > p > span.txt', "Testing Nightwatch here")
+        .getText('div > div:nth-of-type(6) > div > p > span.txt', function(result) {
+            if(result.value === "Testing Nightwatch here") {
+              console.log("The matched text is: ", result.value);
+            }
         })
-        // .getText('div.todo-item', function(result) {
-        //     console.log(result);
-        // })
+        .expect.elements('p').count.to.equal(8)
+        browser.saveScreenshot('tests/screenshots/test.png')
         .end();
     }
   };
